94b2905e64119af950b2d642538512ec26646510
EloiseSchachterle <eloises3@nycstudents.net>
2025-09-25 00:12:17 -0400

    Dog Walker Final

--- src/DogWalkCompany.java
+++ src/DogWalkCompany.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DogWalkCompany {
     private int[] schedule;
     public DogWalkCompany() {
@@ -9,4 +11,33 @@ public class DogWalkCompany {
             s += i + " " + schedule[i] + "\n";
         return s;
     }
+    public int addDogs()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("What hour?");
+        int hour = s.nextInt();
+        System.out.println("How many dogs?");
+        int dogs = s.nextInt();
+        schedule[hour] = dogs;
+        return hour;
+    }
+    /**
+     * Decreases, by numberDogsWalked, the number of dogs available for a walk
+     * during the time specified by hour
+     * Preconditions: 0 <= hour <= 23
+     * numberDogsWalked > 0
+     */
+    public void updateDogs(int hour, int numberDogsWalked){
+        schedule[hour] -= numberDogsWalked;
+    }
+    /**
+     * Returns the number of dogs, always greater than 0, that are available
+     * for a walk during the time specified by hour
+     * Precondition: 0 <= hour <= 23
+     */
+    public int numAvailableDogs(int hour)
+    {
+        return schedule[hour];
+    }
+
 }
